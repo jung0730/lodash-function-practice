83962b543969c2a4980d96216c444efca30964f2
jung0730 <arj2763@gmail.com>
2023-12-12 18:08:03 +0800

    chunk.js solution 2

--- chunk.js
+++ chunk.js
@@ -1,12 +1,24 @@
+// const chunk = (array, size) => {
+//   const length = array.length
+//   const result = new Array(Math.ceil(length / size)); //[, ]
+//   let index = 0
+//   let resIndex = 0
+//   while (index < length) {
+//     result[resIndex++] = array.slice(index, index += size) //含start不含end
+//     // result[0] = [a,b]
+//     // result[1] = [c,d]
+//   }
+//   return result
+// }
+
 const chunk = (array, size) => {
-  const length = array.length
-  const result = new Array(Math.ceil(length / size)); //[, ]
-  let index = 0
-  let resIndex = 0
-  while (index < length) {
-    result[resIndex++] = array.slice(index, index += size) //含start不含end
-    // result[0] = [a,b]
-    // result[1] = [c,d]
+  if (!Array.isArray(array) || size < 1) {
+    return []
+  }
+  const result = []
+  for (let i = 0; i < array.length; i += size) {
+    const value = array.slice(i, i + size)
+    result.push(value)
   }
   return result
 }
