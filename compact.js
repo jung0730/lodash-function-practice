7c266dfac1609ab92ed7e3a74bbd39f38e3dac7c
jung0730 <arj2763@gmail.com>
2023-12-12 18:50:43 +0800

    compact.js solution2

--- compact.js
+++ compact.js
@@ -1,5 +1,9 @@
+// const compact = (array) => {
+//   return array.filter(el => !!el)
+// }
+
 const compact = (array) => {
-  return array.filter(el => !!el)
+  return array.filter(Boolean)
 }
 
 console.log(compact([0, 1, false, 2, '', 3]))
