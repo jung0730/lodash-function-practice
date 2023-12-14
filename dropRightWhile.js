4dfb00519171d342c1c6ab5dcdde67f37ddce5af
jung0730 <arj2763@gmail.com>
2023-12-14 18:51:26 +0800

    dropRightWhile.js solution2

--- dropRightWhile.js
+++ dropRightWhile.js
@@ -1,11 +1,20 @@
 const dropRightWhile = (array, predicate) => {
-  let index
-  for (index = array.length - 1; index >= 0 && predicate(array[index], index, array); index--) {
+  let index;
+  for (index = array.length - 1; predicate(array[index], index, array); index--) {
     // Continue iterating while the predicate is true and there are still elements in the array
   }
 
   return array.slice(0, index + 1);
 }
+
+// Solution 2
+// const dropRightWhile = (array, predicate) => {
+//   let index = array.length - 1;
+//   while (index >= 0 && predicate(array[index], index, array)) {
+//     index--;
+//   }
+//   return array.slice(0, index + 1);
+// }
 
 console.log(dropRightWhile([1, 2, 3, 4, 5], (value) => value < 0))
 console.log(dropRightWhile([1, , 3, 4, 5], (value) => value === undefined))
